8a41e9f452b20cde283626a4a221ec8996aecb53
Larry Li <larry.zy.li@gmail.com>
2018-01-04 15:29:41 -0600

    finished regular project

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const strategy = require("./strategy");
+const request = require("request");
 
 const app = express();
-app.use( session({
-  secret: '@nyth!ng y0u w@nT',
-  resave: false,
-  saveUninitialized: false
-}));
+
+app.use(
+    session({
+        secret: "@nyth!ng y0u w@nT",
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+    console.log("user: ", user);
+    const { name, nickname } = user._json;
+    return done(null, { name, nickname });
+});
+passport.deserializeUser((obj, done) => {
+    return done(null, obj);
+});
+
+app.get(
+    "/login",
+    passport.authenticate("auth0", {
+        successRedirect: "/followers",
+        failureRedirect: "/login",
+        failureFlash: true,
+        connection: "github"
+    })
+);
+app.get("/followers", (req, res, next) => {
+    if (!req.user) {
+        return res.redirect("/login");
+    } else {
+        const followersRequest = {
+            url: `https://api.github.com/users/${req.user.nickname}/followers`,
+            headers: {
+                "User-Agent": req.user.name
+            }
+        };
+        request(followersRequest, (error, response, body) => {
+            // console.log(error);
+            return res.status(200).send(body);
+        });
+    }
+});
 
 const port = 3000;
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
---
+++ strategy.js
@@ -0,0 +1,15 @@
+const { domain, clientID, clientSecret } = require("./config");
+const Auth0Strategy = require("passport-auth0");
+
+module.exports = new Auth0Strategy(
+    {
+        domain,
+        clientID,
+        clientSecret,
+        scope: "openid profile",
+        callbackURL: "/login"
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        return done(null, profile);
+    }
+);
